ee09f82e8e5f96ea818b3dc3aeabba54bfeefa5b
isaac <isaac.mauro@gmail.com>
2021-10-20 12:59:22 -0600

    debug

--- shutup/shutup.py
+++ shutup/shutup.py
@@ -51,17 +51,17 @@ class Shutup(commands.Cog):
                 # Copy this commands context, then modify the author to be the bot, otherwise !mute
                 # gets invoked as the user who called !shutup, and you can't mute yourself
                 ctx.author = bot_user
-                await ctx.send(f"invoking mute command as {str(bot_ctx.author)}")
+                await ctx.send(f"invoking mute command as {str(ctx.author)}")
                 await ctx.invoke(self.bot.get_command('mute'), users=[caller], time_and_reason=time_and_reason)
             else:
                 last_use = await self.config.member(ctx.author).last_use.set(now.strftime("%c"))
-                await ctx.send(f"invoking mute command as {str(bot_ctx.author)}")
+                await ctx.send(f"invoking mute command as {str(ctx.author)}")
                 ctx.author = bot_user
                 await ctx.invoke(self.bot.get_command('mute'), users=[user], time_and_reason=time_and_reason)
         else:
             await self.config.member(ctx.author).last_use.set(now.strftime("%c"))
             ctx.author = bot_user
-            await ctx.send(f"invoking mute command as {str(bot_ctx.author)}")
+            await ctx.send(f"invoking mute command as {str(ctx.author)}")
             await ctx.invoke(self.bot.get_command('mute'), users=[user], time_and_reason=time_and_reason)
 
         
