6d16b390ffca8757d6355fb2c554da1b78ad7706
isaac <isaac.mauro@gmail.com>
2021-10-19 20:14:22 -0600

    idiot

--- shutup/shutup.py
+++ shutup/shutup.py
@@ -30,7 +30,7 @@ class Shutup(commands.Cog):
         '''
         length = self.config.guild(ctx.guild).length
         await ctx.send(f"Muting {user} for {length}")
-        await ctx.invoke(self.bot.get_command('mute'), users=[user], time_and_reason=length)
+        await ctx.invoke(self.bot.get_command('mute'), users=[user], time_and_reason=str(length))
 
         
     @shutup.command()
