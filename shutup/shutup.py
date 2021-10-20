3f92f763fb75938c24a5144a54a343e640c06010
isaac <isaac.mauro@gmail.com>
2021-10-20 08:16:20 -0600

    fix typo

--- shutup/shutup.py
+++ shutup/shutup.py
@@ -45,7 +45,7 @@ class Shutup(commands.Cog):
             if last_use is not None:
                 delta_hours = divmod((now - datetime.strptime(last_use, "%c")).total_seconds(), 3600)[0]
                 if delta_hours < cooldown:
-                    bot_ctx = await ctx.reply("Nice try kid, shutup is on cooldown :mirror:")
+                    bot_ctx = await ctx.reply("Nice try kid, shutup is on cooldown :mirror:").get_context()
                     await bot_ctx.invoke(self.bot.get_command('mute'), users=[ctx.author], time_and_reason=time_and_reason)
                 else:
                     last_use = await self.config.member(ctx.author).last_use.set(now.strftime("%c"))
